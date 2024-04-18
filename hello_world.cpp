b1d2df96671e12fbb90520fe1fb692c9405f9d51
meguaii <uriydronov2006@gmail.com>
2024-04-18 11:08:12 +0300

    without using namespace

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
-#include <string>
-using namespace std;
-int main(){
-    cout<<"your name : ";
-    string name;
-    cin>>name;
-    cout<<"Hello world from "<<name<<endl;
 
+int main() {
+    std::cout << "Hello world!" << std::endl;
+    return 0;
 }
